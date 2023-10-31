cf2bcee8725fdcc7853ad0832f835d53f5d75a84
Di Song <dsong4@slb.com>
2023-10-31 14:55:46 +0800

    feat: add accomplish

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,49 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    if (this.isParamsValid(start, end)) {
+      return this.generateMultiplicationTable(start, end)
+    }
+    return ''
   }
+
+  private isStartNotGraterThanEnd(start: number, end: number): boolean {
+    return start <= end
+  }
+
+  private isParamInRange(param: number): boolean {
+    return param >= 1 && param <= 1000
+  }
+
+  private isParamsValid(start: number, end: number): boolean {
+    return this.isParamInRange(start) && this.isParamInRange(end) && this.isStartNotGraterThanEnd(start, end) && Number.isInteger(start) && Number.isInteger(end)
+  }
+
+  private generateMultiplicationTable(start: number, end: number): string {
+    const lineList = this.generateAllLineList(start, end)
+    return this.mergeAllLineList(lineList)
+  }
+
+  private generateOneLine(start: number, end: number): string {
+    let line = ''
+    for (let i = start; i <= end; i++) {
+      line += `${i}*${end}=${i*end}`
+      if (i !== end) {
+        line += ' '
+      }
+    }
+    return line
+  }
+
+  private generateAllLineList(start: number, end: number): string[] {
+    const lineList = []
+    for (let i = start; i <= end; i++) {
+      lineList.push(this.generateOneLine(start, i))
+    }
+    return lineList
+  }
+
+  private mergeAllLineList(lineList: string[]): string {
+    return lineList.join('\n')
+  }
+
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -13,4 +13,69 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  it('should render multiplication table of (1, 2)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 1
+    const end = 2
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('1*1=1\n1*2=2 2*2=4')
+  })
+
+  it('should render return empty string when start is greater than end', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
+  it('should render return empty string when start is less than 1', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 0
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
+  it('should render return empty string when end is greater than 1000', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 1
+    const end = 1001
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
+  it('should render return empty string when start or end not integer', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 1.1
+    const end = 100
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
 })
